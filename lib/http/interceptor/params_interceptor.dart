b7072cef7739c3a4e8d1a5a89ff789b74f4e0434
ssk <senkun.shi@get88.cn>
2024-02-23 09:38:02 +0800

    更新project_token

--- lib/http/interceptor/params_interceptor.dart
+++ lib/http/interceptor/params_interceptor.dart
@@ -10,7 +10,7 @@ class ParamsInterceptor extends Interceptor {
     if(token != null) {
       headers['token'] = token;
     }
-    headers['project_token'] = '21355A8E631349F5B53AC4B435C44CA0';
+    headers['project_token'] = '42AA73E97A2F49EF8EBB8FE20414072E';
     headers['uk'] = '59a0378351f30f4384c5ecf0985b5c52';
     headers['channel'] = 'cretin_open_api';
     headers['app'] = '1.0.0;1;13';
